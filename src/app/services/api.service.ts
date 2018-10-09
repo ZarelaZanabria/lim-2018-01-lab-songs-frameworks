312b28060c92edae4257a42556ec68c26e68e3d7
Zarela Zanabria <zarelixzana@gmail.com>
2018-10-09 14:23:23 -0500

    Htpps para la api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -17,7 +17,7 @@ export class ApiService {
   //Cremos un funcion para llamar los datos del archivo Json para obtener la lista de musica segun un busqueda
   //Es un observable ();
   searchMusic(artistName: string, queryType: string) {
-    return this.http.get(`http://ws.audioscrobbler.com/2.0/?method=artist.${queryType}&artist=${artistName}&api_key=${this.API_KEY_LASTFM}&format=json`);
+    return this.http.get(`https://ws.audioscrobbler.com/2.0/?method=artist.${queryType}&artist=${artistName}&api_key=${this.API_KEY_LASTFM}&format=json`);
    /*  http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=YOUR_API_KEY&format=json */
   }
 }
